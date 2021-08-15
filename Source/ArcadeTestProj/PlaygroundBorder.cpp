07e14fc37032f30359c182891e75821ab75e1e79
Forik <egor.prokofev.ru@gmail.com>
2021-08-15 23:24:32 +0300

    Добавлен Actor PlaygroundBorder Который удаляет всё объекты, выходящие из его зоны

---
+++ Source/ArcadeTestProj/PlaygroundBorder.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlaygroundBorder.h"
+
+// Sets default values
+APlaygroundBorder::APlaygroundBorder()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APlaygroundBorder::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlaygroundBorder::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
