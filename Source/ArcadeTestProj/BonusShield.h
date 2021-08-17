1827dfa55400ef53a4cbd416bbcd6d53b83d033d
Forik <egor.prokofev.ru@gmail.com>
2021-08-17 11:54:07 +0300

    Добавлены два класса: BonusShield и PawnShield

---
+++ Source/ArcadeTestProj/BonusShield.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Bonus.h"
+#include "BonusShield.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCADETESTPROJ_API ABonusShield : public ABonus
+{
+	GENERATED_BODY()
+	
+};
