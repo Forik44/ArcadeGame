f80bfd9b8c53336c1873758583dfdb680cb8565a
Forik <egor.prokofev.ru@gmail.com>
2021-08-18 20:40:56 +0300

    Добавлен BonusShootLevelUp

---
+++ Source/ArcadeTestProj/BonusShootLevelUp.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Bonus.h"
+#include "BonusShootLevelUp.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCADETESTPROJ_API ABonusShootLevelUp : public ABonus
+{
+	GENERATED_BODY()
+	
+};
