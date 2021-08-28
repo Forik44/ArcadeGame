e0796c2944185b089ef8c31cffd84c8aaf5397b6
Forik <egor.prokofev.ru@gmail.com>
2021-08-28 15:09:41 +0300

    Добавен новый Бонус, исправлено большое количевство багов

---
+++ Source/ArcadeTestProj/BonusHeart.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Bonus.h"
+#include "BonusHeart.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARCADETESTPROJ_API ABonusHeart : public ABonus
+{
+	GENERATED_BODY()
+	
+};
